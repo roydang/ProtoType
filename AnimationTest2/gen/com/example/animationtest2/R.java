d3ea57cb1b8c5d9ac0fc753cc4a1ecffd5463c0f
최형순 <@RoyNote>
2013-10-03 00:01:42 +0900

    바운스 애니메이션

--- AnimationTest2/gen/com/example/animationtest2/R.java
+++ AnimationTest2/gen/com/example/animationtest2/R.java
@@ -11,6 +11,8 @@ public final class R {
     public static final class anim {
         public static final int appear=0x7f040000;
         public static final int disappear=0x7f040001;
+        public static final int enter=0x7f040002;
+        public static final int exit=0x7f040003;
     }
     public static final class attr {
     }
@@ -31,9 +33,13 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int badge_01=0x7f090003;
+        public static final int action_settings=0x7f090008;
+        public static final int badge_01=0x7f090005;
+        public static final int badge_02=0x7f090006;
+        public static final int badge_03=0x7f090007;
         public static final int button1=0x7f090002;
+        public static final int button2=0x7f090003;
+        public static final int button3=0x7f090004;
         public static final int container=0x7f090000;
         public static final int section_label=0x7f090001;
     }
--- AnimationTest2/src/com/example/animationtest2/PopUpTestFragment.java
+++ AnimationTest2/src/com/example/animationtest2/PopUpTestFragment.java
@@ -6,10 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.CycleInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.Keyframe;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.PropertyValuesHolder;
+import com.nineoldandroids.animation.ValueAnimator;
 
 
 
@@ -48,6 +58,103 @@ public class PopUpTestFragment extends Fragment {
 				}
 			}
 		});
+		Button startAni2 = (Button)rootView.findViewById(R.id.button2);
+		startAni2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ImageView badge01 = (ImageView)rootView.findViewById(R.id.badge_02);
+				if (!isClick) {
+					
+					Animation appearAni = AnimationUtils.loadAnimation(getActivity(), R.anim.enter);
+					badge01.setAnimation(appearAni);
+					badge01.setVisibility(View.VISIBLE);
+	//				badge01.startAnimation(appearAni);
+					isClick = true;
+				} else {
+					
+					Animation appearAni = AnimationUtils.loadAnimation(getActivity(), R.anim.exit);
+					badge01.setAnimation(appearAni);
+					badge01.setVisibility(View.GONE);
+	//				badge01.startAnimation(appearAni);
+					isClick = false;
+				}
+			}			
+		});
+		Button startAni3 = (Button)rootView.findViewById(R.id.button3);
+		startAni3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ImageView badge03 = (ImageView)rootView.findViewById(R.id.badge_03);
+			 
+					
+					startAnimation();
+			 
+			}
+		});
+		
+		
+	}
+	
+	public void nineOldAni() {
+		ImageView ball = (ImageView)rootView.findViewById(R.id.badge_03);
+
+		  PropertyValuesHolder pvhW = PropertyValuesHolder.ofFloat("width", ball.getWidth(),   ball.getWidth() * 2);
+          PropertyValuesHolder pvhH = PropertyValuesHolder.ofFloat("height", ball.getHeight(),  ball.getHeight() * 2);
+          PropertyValuesHolder pvTX = PropertyValuesHolder.ofFloat("x", ball.getX(),  ball.getX() - BALL_SIZE/2f);
+          PropertyValuesHolder pvTY = PropertyValuesHolder.ofFloat("y", ball.getY(),  ball.getY() - BALL_SIZE/2f);
+          ObjectAnimator whxyBouncer = ObjectAnimator.ofPropertyValuesHolder(ball, pvhW, pvhH,  pvTX, pvTY).setDuration(DURATION/2);
+          whxyBouncer.setRepeatCount(1);
+          whxyBouncer.setRepeatMode(ValueAnimator.REVERSE);
+          bounceAnim = new AnimatorSet();
+          ((AnimatorSet)bounceAnim).playTogether(whxyBouncer);
+	}
+	public void nineOldAni2() {
+		ImageView ball = (ImageView)rootView.findViewById(R.id.badge_03);
+
+		PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("y", ball.getY(), ball.getHeight() - BALL_SIZE);
+          float ballX = ball.getX();
+          Keyframe kf0 = Keyframe.ofFloat(0f, ballX);
+          Keyframe kf1 = Keyframe.ofFloat(.5f, ballX + 100f);
+          Keyframe kf2 = Keyframe.ofFloat(1f, ballX + 50f);
+          PropertyValuesHolder pvhX = PropertyValuesHolder.ofKeyframe("x", kf0, kf1, kf2);
+          ObjectAnimator yxBouncer = ObjectAnimator.ofPropertyValuesHolder(ball, pvhY, pvhX).setDuration(DURATION/2);
+          yxBouncer.setRepeatCount(1);
+          yxBouncer.setRepeatMode(ValueAnimator.REVERSE);
+          bounceAnim = new AnimatorSet();
+          ((AnimatorSet)bounceAnim).playTogether(yxBouncer);
 	
 	}
+	public void nineOldAni3() {
+		ImageView ball = (ImageView)rootView.findViewById(R.id.badge_03);
+        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("y", ball.getY(), ball.getHeight() - BALL_SIZE);
+        PropertyValuesHolder pvhAlpha = PropertyValuesHolder.ofFloat("alpha", 1.0f, 0f);
+        ObjectAnimator yAlphaBouncer = ObjectAnimator.ofPropertyValuesHolder(ball,   pvhY, pvhAlpha).setDuration(DURATION/2);
+        yAlphaBouncer.setInterpolator(new AccelerateInterpolator());
+        yAlphaBouncer.setRepeatCount(1);
+        yAlphaBouncer.setRepeatMode(ValueAnimator.REVERSE);
+          bounceAnim = new AnimatorSet();
+          ((AnimatorSet)bounceAnim).playTogether(yAlphaBouncer);
+	
+	}	
+	public void nineOldAni4() {
+		ImageView ball = (ImageView)rootView.findViewById(R.id.badge_03);
+        ObjectAnimator yBouncer = ObjectAnimator.ofFloat(ball, "y",  ball.getY(), ball.getHeight() - BALL_SIZE).setDuration(DURATION);
+        yBouncer.setInterpolator(new CycleInterpolator(2));
+
+          bounceAnim = new AnimatorSet();
+          ((AnimatorSet)bounceAnim).playTogether(yBouncer);
+	
+	}	
+	
+	 
+	 float BALL_SIZE = 100f;
+	 int DURATION = 1500;
+	Animator bounceAnim;
+    public void startAnimation() {
+    	nineOldAni4();
+        bounceAnim.start();
+    }
+    
 }
